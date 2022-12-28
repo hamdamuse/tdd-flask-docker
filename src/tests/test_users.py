2b183a060145a0db2a59bb7920809c74f0106878
Hamda Muse <hmuse300@gmail.com>
2022-12-28 19:48:46 +0000

    add route handler for users

---
+++ src/tests/test_users.py
@@ -0,0 +1,16 @@
+import json
+
+
+def test_add_user():
+    client = test_app.test_client()
+    resp = client.post('/users',
+    data=json.dumps({
+        'username': 'Hamda',
+        'email': 'hamda@hamda.com'}),
+        content_type='application/json'
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 201
+    assert 'hamda.hamda.com was added' in data['message']
+    
+
